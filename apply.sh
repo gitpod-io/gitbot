964a3386d909c35102fa197192d5c9e03706fb94
Christian Weichel <chris@gitpod.io>
2021-07-15 13:59:58 +0000

    Integrate gitpod-io with groundwork and PM

--- apply.sh
+++ apply.sh
@@ -8,3 +8,6 @@ cd -
 
 kubectl create configmap plugins --from-file=plugins.yaml=config/plugins.yaml --dry-run -o yaml | kubectl replace configmap plugins -f -
 kubectl create configmap config --from-file=config.yaml=config/config.yaml --dry-run -o yaml | kubectl replace configmap config -f -
+
+kubectl create configmap projectmanager --from-file=projectmanager.yaml=config/projectmanager.yaml --dry-run -o yaml | kubectl replace configmap projectmanager -f -
+kubectl create configmap groundwork --from-file=groundwork.yaml=config/groundwork.yaml --dry-run -o yaml | kubectl replace configmap groundwork -f -
