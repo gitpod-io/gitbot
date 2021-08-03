ebf5b5fee5e8b1f510bb68d0d474a6cd53d2a333
ArthurSens <arthursens2005@gmail.com>
2021-08-03 18:38:07 +0000

    Replacing plugin with periodic prow-job
    Signed-off-by: ArthurSens <arthursens2005@gmail.com>

--- plugins/postmortem-reminder/plugin/config.go
+++
@@ -1,9 +0,0 @@
-package plugin
-
-type Config struct {
-	OrgRepos map[string]RepoConfig `json:"orgRepos" yaml:"orgRepos"`
-}
-
-type RepoConfig struct {
-	slackWebhookUrl string `json:"slackWebhookUrl" yaml:"slackWebhookUrl"`
-}
