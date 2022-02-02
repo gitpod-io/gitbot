b885c404dc757192754836abbf2916bd1edcf0c2
Mads Hartmann <mads379@gmail.com>
2022-02-02 14:15:08 +0000

    Remove groundwork and projectmanager

--- apply.sh
+++ apply.sh
@@ -10,9 +10,6 @@ kubectl create configmap plugins --from-file=plugins.yaml=config/plugins.yaml --
 kubectl create configmap config --from-file=config.yaml=config/config.yaml --dry-run=client -o yaml | kubectl replace configmap config -f -
 
 # note: ensure these config maps already exist
-
-kubectl create configmap projectmanager --from-file=projectmanager.yaml=config/projectmanager.yaml --dry-run=client -o yaml | kubectl replace configmap projectmanager -f -
-kubectl create configmap groundwork --from-file=groundwork.yaml=config/groundwork.yaml --dry-run=client -o yaml | kubectl replace configmap groundwork -f -
 kubectl create configmap customlabels --from-file=customlabels.yaml=config/customlabels.yaml --dry-run=client -o yaml | kubectl replace configmap customlabels -f -
 kubectl create configmap observer --from-file=observer.yaml=config/observer.yaml --dry-run=client -o yaml | kubectl replace configmap observer -f -
 kubectl create configmap willkommen --from-file=willkommen.yaml=config/willkommen.yaml --dry-run=client -o yaml | kubectl replace configmap willkommen -f -
