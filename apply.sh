5a962f319daa5db2b4339bcea244457996683125
Mads Hartmann <mads379@gmail.com>
2022-02-03 10:28:16 +0000

    Remove customlabels

--- apply.sh
+++ apply.sh
@@ -10,5 +10,4 @@ kubectl create configmap plugins --from-file=plugins.yaml=config/plugins.yaml --
 kubectl create configmap config --from-file=config.yaml=config/config.yaml --dry-run=client -o yaml | kubectl replace configmap config -f -
 
 # note: ensure these config maps already exist
-kubectl create configmap customlabels --from-file=customlabels.yaml=config/customlabels.yaml --dry-run=client -o yaml | kubectl replace configmap customlabels -f -
 kubectl create configmap observer --from-file=observer.yaml=config/observer.yaml --dry-run=client -o yaml | kubectl replace configmap observer -f -
